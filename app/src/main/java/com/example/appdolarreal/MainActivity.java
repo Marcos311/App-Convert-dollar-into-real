35e6f9c83b15b000e79e2e0534a0f7bcf64fdd1a
Marcos_bat <marcos.vinisabino@gmail.com>
2021-07-31 16:00:14 -0300

    update

--- app/src/main/java/com/example/appdolarreal/MainActivity.java
+++ app/src/main/java/com/example/appdolarreal/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.appdolarreal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtValorDolar, edtQuantidDolar;
+    TextView txtValorReal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtValorDolar = findViewById(R.id.edtValorDolar);
+        edtQuantidDolar = findViewById(R.id.edtQuantidDolar);
+
+        txtValorReal = findViewById(R.id.txtValorReal);
+
+    }
+
+    public void calcular(View view) {
+
+        double quantDolar = Double.parseDouble(edtQuantidDolar.getText().toString());
+        double valorDolar = Double.parseDouble(edtValorDolar.getText().toString());
+
+        double totalReais = valorDolar * quantDolar;
+
+        DecimalFormat decimal = new DecimalFormat("0.00");
+        String valorFormatado = decimal.format(totalReais);
+
+        txtValorReal.setText("R$: " + valorFormatado);
+
+
     }
 }
